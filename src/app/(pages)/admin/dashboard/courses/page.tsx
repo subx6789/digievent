ffe42236a3d63141792ec177f6f1fed25904a5e3
subx6789 <subhajits956@gmail.com>
2025-04-30 21:02:12 +0530

    feat: Fix some bug

--- src/app/(pages)/admin/dashboard/courses/page.tsx
+++ src/app/(pages)/admin/dashboard/courses/page.tsx
@@ -138,7 +138,7 @@ const AdminCollegeCourses = () => {
             <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {courses.map((course) => (
                 <CourseCard
-                  key={course.course}
+                  key={course.courseName}
                   course={course}
                   onEdit={handleEditCourse}
                   onRemove={handleRemoveCourse}
